5389b7b1b6c521197cc634fd4c9afa144227e8b3
Omkar Mahajan <omkar.mahajan@sarasanalytics.com>
2021-11-21 14:36:59 +0530

    finished

--- src/main/java/com/example/springsecurityjwt/HelloResource.java
+++ src/main/java/com/example/springsecurityjwt/HelloResource.java
@@ -1,13 +1,47 @@
 package com.example.springsecurityjwt;
 
+import com.example.springsecurityjwt.models.AuthenticationRequest;
+import com.example.springsecurityjwt.models.AuthenticationResponse;
+import com.example.springsecurityjwt.services.MyUserDetailsService;
+import com.example.springsecurityjwt.util.JwtUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class HelloResource {
   
-  @RequestMapping("/hello")
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private MyUserDetailsService userDetailsService;
+
+  @Autowired
+  private JwtUtil jwtTokenUtil;
+
+  @GetMapping("/hello")
   public String hello() {
     return "Hello World";
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+    authenticationManager.authenticate(
+      new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+    );
+
+    final UserDetails UserDetails = userDetailsService
+      .loadUserByUsername(authenticationRequest.getUsername());
+    final String jwt = jwtTokenUtil.generateToken(UserDetails);
+
+    return ResponseEntity.ok(new AuthenticationResponse(jwt));
   }
 }
